1004e1dd0e95b327cf7c67f0ea1fd5ae7ad50c36
similar2 <shimile18@gmail.com>
2024-12-09 21:13:40 +0800

    init

---
+++ src/main/java/org/example/java2final/Java2FinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.java2final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/java2final/Java2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java2final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
